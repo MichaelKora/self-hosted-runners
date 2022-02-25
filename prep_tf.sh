491c4bb07fdb645f12fd3c1fff08aec4f99d0468
michaelkora <michael.koras0@gmail.com>
2022-02-25 18:40:12 +0100

    start instances and run job 7.5

--- prep_tf.sh
+++ prep_tf.sh
@@ -8,10 +8,4 @@ export TF_VAR_password=$OS_PASSWORD
 export TF_VAR_user_domain_name=$OS_USER_DOMAIN_NAME
 export TF_VAR_tenant_name=$OS_TENANT_NAME
 export TF_VAR_tenant_id=$OS_TENANT_ID
-
-
-# Set image + flavour
-#export TF_VAR_ansible_user="debian"
-#export TF_VAR_image_name="Debian 11"
-#export TF_VAR_flavor_name="d2-8"
-
+export TF_VAR_region="DE1"
--- set_env.sh
+++ set_env.sh
@@ -8,19 +8,9 @@ export OS_USER_DOMAIN_NAME="Default"
 export OS_PROJECT_DOMAIN_NAME="Default"
 export TF_VAR_auth_url=$OS_AUTH_URL
 EOF
-
 echo export OS_TENANT_ID=$1 >> .env
 echo OS_TENANT_NAME=$2 >> .env
 echo export OS_USERNAME=$3 >> .env
 echo export OS_PASSWORD=$4 >> .env
 echo export TF_VAR_keypair_name= $5 >> .env
-
-echo export TF_VAR_region="DE1" >> .env
-echo export TF_VAR_user_name=$OS_USERNAME >> .env
-echo export TF_VAR_password=$OS_PASSWORD >> .env
-echo export TF_VAR_user_domain_name=$OS_USER_DOMAIN_NAME >> .env
-echo export TF_VAR_tenant_name=$OS_TENANT_NAME >> .env
-echo export TF_VAR_tenant_id=$OS_TENANT_ID >> .env
-echo export TF_VAR_ansible_user=$6 >> .env
-
 source .env
