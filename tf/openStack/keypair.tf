108febae961da66dae57d29a37398fa80146877d
michaelkora <michael.koras0@gmail.com>
2022-02-26 19:40:49 +0100

    start instances and run job 8.04

--- tf/openStack/keypair.tf
+++ tf/openStack/keypair.tf
@@ -6,6 +6,6 @@ resource "openstack_compute_keypair_v2" "githubrunner" {
   count = var.server_count
   provider   = openstack
   region     = var.regions[ count.index ]
-  name       = var.keypair_name
+  name       = "${var.keypair_name}-${count.index}"
   public_key = file("../../id_rsa.pub")
 }
