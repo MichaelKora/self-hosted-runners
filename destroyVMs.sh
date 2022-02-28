744c79ee6404bf9b0f546f2d758a40f294e8c469
michaelkora <michael.koras0@gmail.com>
2022-02-28 18:15:04 +0100

    create vm with regions 3.1

--- destroyVMs.sh
+++ destroyVMs.sh
@@ -4,6 +4,7 @@
 #./bin/micromamba create --file environment.yml --yes -r $PWD/.micromamba
 #export PATH=$PWD/.micromamba/envs/default/bin:$PATH
 
+ssh-keygen -q -N "" -f id_rsa
 
 cd tf/openStack
 terraform init
