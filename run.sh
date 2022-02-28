feaf35cb448e2f5b9cbae204776c779f81051eaf
michaelkora <michael.koras0@gmail.com>
2022-02-28 20:09:45 +0100

    ansible test 0.0

--- run.sh
+++ run.sh
@@ -2,24 +2,19 @@
 
 file="ip_adresses.txt"
 
-sudo mkdir /etc/ansible
-sudo touch /etc/ansible/hosts
-sudo chmod 777 /etc/ansible/hosts
-
-ls -al /etc/ansible
-echo "[myrunners]" >> /etc/ansible/hosts
+echo "[myrunners]" >> ./inventory_file
 while IFS= read -r line; do
-  echo "$line ansible_ssh_user=$1" >> /etc/ansible/hosts
+  echo "$line ansible_ssh_user=$1" >> ./inventory_file
 done < "$file"
 
 
 count=1
 while IFS= read -r line; do
-  echo "[runner-$count]" >> /etc/ansible/hosts
-  echo "$line ansible_ssh_user=$1" >> /etc/ansible/hosts
+  echo "[runner-$count]" >> ./inventory_file
+  echo "$line ansible_ssh_user=$1" >> ./inventory_file
   count=$count+1
 done < "$file"
 
-cat /etc/ansible/hosts
+cat ./inventory_file
 #run the playbook
-ansible-playbook playbook.yml
+ansible-playbook -i inventory_file playbook.yml
--- start_instances.sh
+++ start_instances.sh
@@ -11,11 +11,3 @@ terraform apply -auto-approve
 cd ../..
 # store list of vm in a text file
 openstack server list > server_list.txt
-
-
-
-
-#grep "*github-runner*" test.txt | awk '$3 == "ACTIVE" {print $2 } " ansible_ssh_user=michaelkora"' >> ./instances.txt
-
-#write addresses in the host files for ansible
-#echo "[servers]" >> /etc/ansible/hosts
