fe034d540a64c65da59a4b3ec1ee853b46aca3a6
michaelkora <michael.koras0@gmail.com>
2022-02-28 20:30:40 +0100

    ansible test 0.4

--- run.sh
+++ run.sh
@@ -4,14 +4,14 @@ file="ip_adresses.txt"
 
 echo "[myrunners]" >> ./inventory_file
 while IFS= read -r line; do
-  echo "$line ansible_ssh_user=$1" >> ./inventory_file
+  echo "$line" >> ./inventory_file
 done < "$file"
 
 
 declare -i count=1
 while IFS= read -r line; do
   echo "[runner-$count]" >> ./inventory_file
-  echo "$line ansible_ssh_user=$1" >> ./inventory_file
+  echo "$line" >> ./inventory_file
   count=$count+1
 done < "$file"
 
