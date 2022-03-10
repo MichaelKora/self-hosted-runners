4b054ab72f789cd647189eb6231fa6df3517eb65
michaelkora <michael.koras0@gmail.com>
2022-03-10 13:38:38 +0100

    code cleaning 0.3

--- destroyVMs.sh
+++ destroyVMs.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
 echo "***delete scripts..."
+
 ##     manual deletion
 #openstack server list > server_list.txt
 #python delete.py
-#echo "after delete"
-#openstack server list
 
-#openstack server delete github-runner-0
-#ssh-keygen -q -N "" -f id_rsa
 cd tf/openstack
 
 echo "***************** ls -al tf/openstack****************************"
--- mamba_env.sh
+++ mamba_env.sh
@@ -2,9 +2,9 @@
 #!/bin/bash
 #Create mamba environment
 
-wget -qO- https://micromamba.snakepit.net/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
-./bin/micromamba create --file environment.yml --yes -r $PWD/.micromamba
-export PATH=$PWD/.micromamba/envs/default/bin:$PATH
+#wget -qO- https://micromamba.snakepit.net/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
+#./bin/micromamba create --file environment.yml --yes -r $PWD/.micromamba
+#export PATH=$PWD/.micromamba/envs/default/bin:$PATH
 
 
 #          sudo apt update
