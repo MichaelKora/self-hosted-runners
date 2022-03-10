877cb320a0c0968a016c6cfbbe726127d409371c
michaelkora <michael.koras0@gmail.com>
2022-03-10 14:12:39 +0100

    code cleaning 0.5

--- destroyVMs.sh
+++ destroyVMs.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-echo "***delete scripts..."
-
-##     manual deletion
-#openstack server list > server_list.txt
-#python delete.py
-
 cd tf/openstack
-
-echo "***************** ls -al tf/openstack****************************"
-ls -al
-echo "++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "**** current tfstate ****"
 cat terraform.tfstate
-
+echo "**** terraform apply -destroy -auto-approve ****"
 terraform apply -destroy -auto-approve
--- run.sh
+++ run.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 
-#file="ip_adresses.txt"
-# store list of vm in a text file
-openstack server list > server_list.txt
-cat server_list.txt
-
-#python setup.py
-
-
-# copy the key of the runner in the vm in order to be able to reach them using ssh and therefore ansible
-#echo "***copying runner ssh key to new created instances..."
-#while IFS= read -r line; do
-#    echo $line
-#    ssh-copy-id -i id_rsa.pub ubuntu@$line
-#done < "$file"
-#echo "***ssh key copied!"
+openstack server list 
 
 #show ansibe terraform inventory
 echo "***ansible commands ... "
